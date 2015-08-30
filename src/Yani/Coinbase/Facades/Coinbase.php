5688c3755d2cdf300d5afd0a7d5a3f4cfdf480f7
Yani Iliev <yani@iliev.me>
2015-08-30 01:34:00 +0000

    Add Coinbase Facade
    This is used to make the life of the developer easier. Coinbase service
    can be invoice using `Coinbase` Facade. To create a new order, one can
    call `Coinbase::createOrder($amount, $currency, $name)`.

---
+++ src/Yani/Coinbase/Facades/Coinbase.php
@@ -0,0 +1,16 @@
+<?php namespace Yani\Coinbase\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Coinbase extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return 'coinbase';
+	}
+}
